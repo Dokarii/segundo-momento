d5c973bdc72673302bc079a805e015d0c9fd494e
Dokarii <ricardotejedor18@gmail.com>
2025-05-10 14:05:57 -0500

    Registro terminado

--- src/Pages/Registro.tsx
+++ src/Pages/Registro.tsx
@@ -1,7 +1,7 @@
-import "./Login.css";
+import "./Registro.css";
 import {Link } from "react-router-dom";
 
-const Login = () => {
+const Registro = () => {
   return (
     <div className="form-container">
       <p className="title">Iniciar sesión</p>
@@ -23,22 +23,40 @@ const Login = () => {
             id="password"
             placeholder="Ingresa tu contraseña"
           />
+        </div>
+        <div className="input-group">
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Ingresa tu nombre"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Correo</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Ingresa tu correo"
+          />
           <div className="forgot">
             <p></p>
           </div>
         </div>
-        <button className="sign" style={{ marginBottom: "20px" }}>
-          Iniciar Sesion
+        <button className="sign" style={{ marginBottom: "10px" }}>
+          Registrar
         </button>
       </form>
-      <p className="signup" style={{ marginTop: "20px" }}>
-        <a rel="noopener noreferrer" href="#" className="signup-link">
+      <p className="signup">
+        <a rel="noopener noreferrer" href="" className="signup-link">
           {" "}
-          <Link to="/registro">¿No tiene una cuenta?</Link>
+          <Link to="/">¿Ya tienes una cuenta?</Link>
         </a>
       </p>
     </div>
   );
 };
 
-export default Login;
+export default Registro;
--- src/Router/Enrutador.tsx
+++ src/Router/Enrutador.tsx
@@ -1,9 +1,15 @@
 import Login from "../Pages/Login";
+import Registro from "../Pages/Registro";
 
 export let enrutador = [
     {
     path: "/",
     element: <Login />
     }
+  ,
+  {
+    path: "/Registro",
+    element: <Registro />
+  }
   ];
   
